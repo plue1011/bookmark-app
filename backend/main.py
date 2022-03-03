d5a5da15a478ebf1519ca4603055c325ebfe75d4
plue1011 <fuekimasao@yahoo.co.jp>
2022-03-03 06:15:58 +0000

    スタートページに必要な情報のAPIを実装

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, FastAPI
 
+from backend.routes.folders import router as startpage_router
 from backend.routes.signup import router as signup_router
 
 router = APIRouter()
 router.include_router(signup_router)
+router.include_router(startpage_router)
 
 app = FastAPI()
 app.include_router(router)
