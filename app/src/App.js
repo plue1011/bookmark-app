2773416c9b1df8f7809a91a8fcba0a4026a974c2
tatsuyukibaba <basketball.bb84@gmail.com>
2022-03-05 10:00:34 +0000

    ブログカード作成

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import {Box} from "@chakra-ui/react"
+import Content from "./Content"
 import Home from "./Home"
 
 export default function App() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/Content" element={<Content/>}/>
       </Routes>
     </BrowserRouter>
   );
