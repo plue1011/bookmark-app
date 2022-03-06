b23f0813566114f8e47e6e845c1f6adaee82f394
tatsuyukibaba <basketball.bb84@gmail.com>
2022-03-06 09:39:30 +0000

    Contentにinline-blockを追記

--- app/src/Content.js
+++ app/src/Content.js
@@ -4,7 +4,7 @@ import {Box,Image,Link,HStack,Heading,Text,Tooltip} from "@chakra-ui/react"
 export default function App(info) {
   return (
     <Tooltip label={info.comment}>
-    <Box width="300px" height="200px" borderWidth="1px" overflow="hidden" borderRadius="10px" p="5px" shadow="md">
+    <Box width="300px" height="200px" borderWidth="1px" overflow="hidden" borderRadius="10px" p="5px" shadow="md" display="inline-block">
       <Link href={info.url}>
       <HStack spacind="1px">
         <Image width="100px" src={info.image} alt="NotImage"/> 
