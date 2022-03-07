7ebb0845e42d3d48df93b197a649a6936f3e3036
yutaDS <minowa.y.7@gmail.com>
2022-03-07 13:19:14 +0000

    sign in, up pageの作成

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import {Box} from "@chakra-ui/react"
+// import {Box} from "@chakra-ui/react"
+import Signin from "./Signin"
+import Signup from "./Signup"
+// import Home from "./Home"
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Box>Hello World</Box>}/>
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home/>}/> */}
+          <Route path="/Signin" element={<Signin/>}/>
+          <Route path="/Signup" element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
