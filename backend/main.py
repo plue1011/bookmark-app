66da4181a62c70d0784c8ebd9015c0675e57e71c
tatsuyukibaba <basketball.bb84@gmail.com>
2022-03-15 15:43:19 +0000

    ChakraProvider追加&CORS対策追加

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from backend.routes.bookmark import router as bookmark_router
 from backend.routes.contents import router as contents_router
 from backend.routes.folders import router as startpage_router
 from backend.routes.sign import router as sign_router
+from starlette.middleware.cors import CORSMiddleware
 
 router = APIRouter()
 router.include_router(sign_router)
@@ -12,4 +13,11 @@ router.include_router(bookmark_router)
 router.include_router(contents_router)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 app.include_router(router)
