94e0c20b0ebf48e6f63d97f072954e0a01d50853
plue1011 <fuekimasao@yahoo.co.jp>
2022-03-18 15:55:52 +0000

    postのテスト

--- backend/models/schemas/sign.py
+++ backend/models/schemas/sign.py
@@ -8,6 +8,11 @@ class SignUp(BaseModel):
     status: bool = Field(description="signupが成功したか否か")
 
 
+class SignUpInput(BaseModel):
+    name: str = Field(description="signupの名前")
+    password: str = Field(description="signupのパスワード")
+
+
 class SignIn(BaseModel):
     status: bool = Field(description="signinが成功したか否か")
     user_id: Optional[UUID] = Field(description="user_id")
