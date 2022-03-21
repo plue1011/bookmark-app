5393a951f11d8831e88483cef9c75e6839cbe682
tatsuyukibaba <basketball.bb84@gmail.com>
2022-03-21 20:13:08 +0000

    ヘッダーで編集保存をできるようにした

--- app/src/Header.js
+++ app/src/Header.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from "react";
 import axios from "axios"
 import {Box,Button,Center,Heading} from "@chakra-ui/react"
 
-export default function Header(){
+export default function Header(edit,setEdit,setPage,setInfo,setInfoCopy,info,infoCopy){
     return(
         <Box
         width="100%"
@@ -13,7 +13,21 @@ export default function Header(){
         box-sizeing="border-box"
         zIndex="sticky"
         >
-          <Heading float="left" color="white">Bookmark</Heading>
+          <Heading float="left" color="white" onClick={() => {
+            setPage("home")
+            setEdit(false)
+            }}
+            _hover={{cursor:'pointer'}}>Bookmark</Heading>
+          {edit
+            ?<Heading float="right" color="white" onClick={() => {
+              setEdit(!edit)
+              setInfo(infoCopy)
+            }} _hover={{cursor:'pointer'}}>Save</Heading> 
+            :<Heading float="right" color="white" onClick={() => {
+              setEdit(!edit)
+              setInfoCopy(info)
+            }} _hover={{cursor:'pointer'}}>Edit</Heading>
+          }
         </Box> 
     )
 }
