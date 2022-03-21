dfae4b259a6860656bd2970bd93d724c7748da2a
tatsuyukibaba <basketball.bb84@gmail.com>
2022-03-21 20:09:54 +0000

    編集できないように変更

--- app/src/Folder.js
+++ app/src/Folder.js
@@ -1,6 +1,5 @@
-import React,{useState,useEffect} from "react";
-import {Box,Button,Center,Heading} from "@chakra-ui/react"
-import {ReactSortable} from "react-sortablejs"
+import React from "react";
+import {Box,Center} from "@chakra-ui/react"
 import axios from "axios"
 
 export default function Folder(group,setGroup,setPage,setInfo){
@@ -16,21 +15,12 @@ export default function Folder(group,setGroup,setPage,setInfo){
       
     return(
         <Box align='center' py="90px" maxWidth="1500px" margin="auto">
-          <ReactSortable
-          list={group}
-          setList={setGroup}
-          animation={150}
-          onChange={(order, sortable, evt) => {}}
-          onEnd={evt => {}}
-          align='center' justify='center'
-          >
-            {group.map((g) => 
-            <Box display="inline-block" margin="10px">
-              <Center onClick={() => ClickFile(g.id)} bg='#CBD5E0' width='200px' height='100px'　borderRadius='10px' _hover={{bg:'#A0AEC0',cursor:'pointer'}}>
-                {g.title}
-              </Center>
-            </Box>)}
-          </ReactSortable>
+          {group.map((g) => 
+          <Box display="inline-block" margin="10px">
+            <Center onClick={() => ClickFile(g.id)} bg='#CBD5E0' width='200px' height='100px'　borderRadius='10px' _hover={{bg:'#A0AEC0',cursor:'pointer'}}>
+              {g.title}
+            </Center>
+          </Box>)}
         </Box>
     )
 }
--- app/src/Set.js
+++ app/src/Set.js
@@ -1,22 +1,13 @@
-import React,{useState} from "react";
-import {Box,Heading} from "@chakra-ui/react"
-import {ReactSortable} from "react-sortablejs"
+import React from "react";
+import {Box} from "@chakra-ui/react"
 import Content from "./Content"
 
 export default function Set(group,setGroup) {
     return (
       <Box align='center' py="90px" maxWidth="1500px" margin="auto">
-      <ReactSortable
-      list={group}
-      setList={setGroup}
-      animation={150}
-      onChange={(order, sortable, evt) => {}}
-      onEnd={evt => {}}
-      >
       {group.map((bookmark) => <Box display="inline-block" padding='10px' margin='auto'>
         {Content(bookmark.info)}
       </Box>)}
-      </ReactSortable>
       </Box>
     );
 }
